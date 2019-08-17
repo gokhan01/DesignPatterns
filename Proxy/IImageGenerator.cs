7f9c30586c4a51bb3c2ad9d3c09e2beeb9d8071d
gokhanuyar <gokhan.uyar@polynomtech.com>
2019-08-17 21:41:43 +0300

    proxy

---
+++ Proxy/IImageGenerator.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Proxy
+{
+    //Subject
+    interface IImageGenerator
+    {
+        void ShowImage();
+    }
+
+    //Real Subject
+    class ImageGenerator : IImageGenerator
+    {
+        private string _fullPath;
+        public ImageGenerator(string fullPath)
+        {
+            _fullPath = fullPath;
+        }
+        public void ShowImage()
+        {
+            Console.WriteLine("{0} gösteriliyor.", _fullPath);
+        }
+    }
+
+    //Proxy
+    class ImageGeneratorProxy : IImageGenerator
+    {
+        private ImageGenerator _generator;
+        private string _fullPath;
+        public ImageGeneratorProxy(string fullPath)
+        {
+            _fullPath = fullPath;
+        }
+
+        public void ShowImage()
+        {
+            if (_generator == null)
+            {
+                //işlem gerçekleşene kadar client tarafında geçici bir görsel gösterilebilir(farklı thread)
+                _generator = new ImageGenerator(_fullPath);
+            }
+
+            _generator.ShowImage();
+        }
+    }
+}
---
+++ Proxy/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Proxy
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ImageGeneratorProxy proxy1 = new ImageGeneratorProxy("Resimler/devops.jpeg");
+
+            ImageGeneratorProxy proxy2 = new ImageGeneratorProxy("Resimler/iskender.jpg");
+
+            proxy1.ShowImage();
+            proxy2.ShowImage();
+            proxy1.ShowImage();
+
+            Console.ReadKey();
+        }
+    }
+}
